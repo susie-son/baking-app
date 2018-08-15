f28befc93e5a540e5bf12316d4ee505c7ff87cf4
susie-son <susieson99@gmail.com>
2018-08-15 12:16:41 -0400

    Implement Timber logging.

--- app/src/main/java/me/susieson/bakingapp/MainActivity.java
+++ app/src/main/java/me/susieson/bakingapp/MainActivity.java
@@ -1,7 +1,9 @@
 package me.susieson.bakingapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import timber.log.Timber;
+import timber.log.Timber.DebugTree;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Timber.plant(new DebugTree());
     }
 }
